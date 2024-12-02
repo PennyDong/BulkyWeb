310cb499808ec9cbb3ffb787f1341ae9eef365f7
Penny <dg127485@gmail.com>
2024-12-02 23:26:12 +0800

    Razor END

--- BulkyWebRazor_Temp/Program.cs
+++ BulkyWebRazor_Temp/Program.cs
@@ -1,25 +1,19 @@
-using BulkyWeb.Data;
+using BulkyWebRazor_Temp.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages();
 //依賴注入
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-/*
- 創建資料庫
- */
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -31,8 +25,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages(); //pages文件中的任何內容都可以
 
 app.Run();
